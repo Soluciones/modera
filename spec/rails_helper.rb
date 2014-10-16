0e930b8745fe3b34d706de2d80be8b398e6267a7
scruti <pringaohowto@gmail.com>
2014-10-16 11:23:19 +0200

    removing unnecessary test requirements

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,12 +5,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-require 'capybara/rspec'
 require 'database_cleaner'
-require 'draper/test/rspec_integration'
-require 'email_spec'
-require 'capybara/poltergeist'
-require 'capybara/rails'
 require 'shoulda-matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
