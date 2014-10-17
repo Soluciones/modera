1aa0bf702222d8fd8af8b56e265e04e6b828142a
scruti <pringaohowto@gmail.com>
2014-10-17 04:57:19 +0200

    more specific non baneable domains validation
    we were setting substrings of non baneable domains as a non baneable themselves.
    Also we were setting subdomains with names that equal to non baneable domain as non baneable themselves.

--- app/models/modera/banea_dominio.rb
+++ app/models/modera/banea_dominio.rb
@@ -1,25 +1,45 @@
 module Modera
   class BaneaDominio < ActiveRecord::Base
-    NO_BANEABLES = %w(gmail yahoo hotmail)
+    NO_BANEABLES = %w(gmail yahoo hotmail outlook live.com telefonica movistar rankia)
 
     validates :dominio, presence: true, uniqueness: true
-    validate :es_baneable
+    validate :dominio_es_baneable
 
     before_validation { dominio.try(:downcase!) }
 
-    def es_baneable
-      return if dominio.blank?
-      formato_extension = /\A\.?[a-z]{1,3}\z/
+    def self.lista
+      pluck(:dominio)
+    end
 
-      if dominio =~ formato_extension
+    protected
+
+    def dominio_es_baneable
+      return if dominio.blank?
+
+      if dominio_con_caracteres_invalidos?
+        errors.add(:dominio, 'El dominio introducido tiene carácters inválidos')
+      elsif dominio_es_una_extension?
         errors.add(:dominio, 'Las extensiones de dominio no se pueden banear')
-      elsif NO_BANEABLES.any? { |no_baneable| dominio.include?(no_baneable) || no_baneable.include?(dominio) }
+      elsif dominio_listado_como_no_baneable?
         errors.add(:dominio, 'Este dominio no puede ser baneado')
       end
     end
 
-    def self.lista
-      pluck(:dominio)
+    def dominio_es_una_extension?
+      dominio =~ /\A\.?[a-z]{1,3}\z/i
+    end
+
+    def dominio_con_caracteres_invalidos?
+      dominio =~ /[^a-z0-9\.-]/i
+    end
+
+    def dominio_listado_como_no_baneable?
+      subdominio = '([a-z\-\d]+\.)'
+      extension = '(\.[a-z]{2,3})'
+
+      NO_BANEABLES.any? do |no_baneable|
+        dominio =~ /\A#{ subdominio }*#{ no_baneable }#{ extension }{,2}\z/i
+      end
     end
   end
 end
--- spec/models/modera/banea_dominio_spec.rb
+++ spec/models/modera/banea_dominio_spec.rb
@@ -3,19 +3,48 @@ require 'rails_helper'
 module Modera
   describe BaneaDominio do
     describe 'validation test' do
+      let(:error_caracteres) { 'El dominio introducido tiene carácters inválidos' }
       let(:error_extension) { 'Las extensiones de dominio no se pueden banear' }
       let(:error_dominio) { 'Este dominio no puede ser baneado' }
 
+      before { stub_const('Modera::BaneaDominio::NO_BANEABLES', %w(gmail yahoo.com)) }
+
       it { is_expected.to validate_presence_of(:dominio).with_message('Dominio no puede estar en blanco') }
+
       it { is_expected.not_to allow_value('gmail').for(:dominio).with_message(error_dominio) }
       it { is_expected.not_to allow_value('gmail.com').for(:dominio).with_message(error_dominio) }
+      it { is_expected.not_to allow_value('gmail.com.mx').for(:dominio).with_message(error_dominio) }
+      it { is_expected.not_to allow_value('subdominio.gmail').for(:dominio).with_message(error_dominio) }
+      it { is_expected.not_to allow_value('subdominio.gmail.com').for(:dominio).with_message(error_dominio) }
+      it { is_expected.not_to allow_value('subdominio.gmail.com.mx').for(:dominio).with_message(error_dominio) }
+
+      it { is_expected.not_to allow_value('yahoo.com').for(:dominio).with_message(error_dominio) }
+      it { is_expected.not_to allow_value('yahoo.com.mx').for(:dominio).with_message(error_dominio) }
+
       it { is_expected.not_to allow_value('.com').for(:dominio).with_message(error_extension) }
       it { is_expected.not_to allow_value('com').for(:dominio).with_message(error_extension) }
       it { is_expected.not_to allow_value('.es').for(:dominio).with_message(error_extension) }
       it { is_expected.not_to allow_value('es').for(:dominio).with_message(error_extension) }
+
+      it { is_expected.not_to allow_value('@gmail.com').for(:dominio).with_message(error_caracteres) }
+      it { is_expected.not_to allow_value('domin?o').for(:dominio).with_message(error_caracteres) }
+      it { is_expected.not_to allow_value('dominio.c_om').for(:dominio).with_message(error_caracteres) }
+
       it { is_expected.to allow_value('dominio').for(:dominio) }
       it { is_expected.to allow_value('dominio.es').for(:dominio) }
-      it { is_expected.to allow_value('dominio.com').for(:dominio) }
+      it { is_expected.to allow_value('subdominio.dominio.com').for(:dominio) }
+      it { is_expected.to allow_value('sub-dominio1.subdominio2.dominio.com').for(:dominio) }
+      it { is_expected.to allow_value('sub-dominio1.subdominio2.dominio').for(:dominio) }
+
+      it { is_expected.to allow_value('gmai').for(:dominio) }
+      it { is_expected.to allow_value('gmai.com').for(:dominio) }
+      it { is_expected.to allow_value('gmail2').for(:dominio) }
+      it { is_expected.to allow_value('gmail2.co.uk').for(:dominio) }
+      it { is_expected.to allow_value('gmail.co.uk.mx').for(:dominio) }
+
+      it { is_expected.to allow_value('yahoo').for(:dominio) }
+      it { is_expected.to allow_value('yahoo.es').for(:dominio) }
+      it { is_expected.to allow_value('yaho.com').for(:dominio) }
     end
   end
 end
