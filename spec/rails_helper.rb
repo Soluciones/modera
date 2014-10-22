a7fbd87bd5c115f4dcecce87420fbfa05b5170cb
scruti <pringaohowto@gmail.com>
2014-10-22 16:33:19 +0200

    Fixing BaneaDominio factory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ require 'rspec/rails'
 
 require 'database_cleaner'
 require 'shoulda-matchers'
+require 'factory_girl_rails'
+require 'ffaker'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
