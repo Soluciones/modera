b58b5e66373a5847a476978338667ebd4eb27e92
scruti <pringaohowto@gmail.com>
2014-10-23 09:39:22 +0200

    blank domain shouldn't format validation

--- app/models/modera/banea_dominio.rb
+++ app/models/modera/banea_dominio.rb
@@ -14,8 +14,6 @@ module Modera
     protected
 
     def dominio_es_baneable
-      return if dominio.blank?
-
       if dominio_con_formato_incorrecto?
         errors.add(:dominio, 'El dominio introducido tiene un formato incorrecto')
       elsif dominio_listado_como_no_baneable?
