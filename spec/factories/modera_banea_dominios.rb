ef34c5861242d729181a41ec1ef8eddb94bc7d14
David BF <davidbf@rankia.com>
2015-07-06 09:27:31 +0200

    Use FFaker intead of Faker

--- spec/factories/modera_banea_dominios.rb
+++ spec/factories/modera_banea_dominios.rb
@@ -2,11 +2,11 @@
 
 FactoryGirl.define do
   factory :modera_banea_dominio, class: 'Modera::BaneaDominio' do
-    dominio         { Faker::Internet.domain_name }
-    updated_usuario { Faker::Internet.user_name }
+    dominio         { FFaker::Internet.domain_name }
+    updated_usuario { FFaker::Internet.user_name }
 
     factory :modera_banea_dominio_sin_extension, class: 'Modera::BaneaDominio' do
-      dominio         { Faker::Internet.domain_word }
+      dominio         { FFaker::Internet.domain_word }
     end
   end
 end
